4d1a3a76a2c3c692a25ddafc800cd9915a40a686
Seweryn Kuliga <skuligas@gmail.com>
2021-10-05 17:06:54 +0200

    Create App
    First commit to build app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* O mnie */}
+      {/* Umiejętności */}
+      
+    </div>
+  );
+}
+
+export default App;
