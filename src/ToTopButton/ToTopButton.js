b921870a9286869cbd37c3f9bf09d18016ab2562
Seweryn Kuliga <skuligas@gmail.com>
2021-10-06 12:28:32 +0200

    Add Scroll To Top Button

--- src/ToTopButton/ToTopButton.js
+++ src/ToTopButton/ToTopButton.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+
+
+
 function ToTopButton() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", toggleVisibility);
+    }, []);
+
     return (
-        <Container>
-            
-        </Container>
+        <>
+            {isVisible && 
+            <Container
+                onClick={scrollToTop}
+            >
+                Przewiń do góry
+            </Container>}
+        </>
     )
 }
 
@@ -15,9 +44,14 @@ const Container = styled.div`
     display: block;
     position: fixed;
 
-    width: 3rem;
-    height: 2rem;
+    width: 6rem;
+    height: 4rem;
     background-color: red;
     bottom: 1.5rem;
     right: 1.5rem;
+    cursor: pointer;
+
+    &:hover {
+        color: red;
+    }
 `
